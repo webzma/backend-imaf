99f3e8e2c1687d69c9a3e86261d417497bbab69d
Wilberk Ledezma <ledezma.wilberk@gmail.com>
2026-04-15 12:03:32 -0400

    Make comprobante_url accessor nullable in Pago model and add Cloudinary filesystem configuration

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,15 @@ return [
             'report' => false,
         ],
 
+        'cloudinary' => [
+            'driver' => 'cloudinary',
+            'url' => env('CLOUDINARY_URL'),
+            'cloud' => env('CLOUDINARY_CLOUD_NAME'),
+            'key' => env('CLOUDINARY_API_KEY'),
+            'secret' => env('CLOUDINARY_API_SECRET'),
+            'secure' => true,
+        ],
+
     ],
 
     /*
